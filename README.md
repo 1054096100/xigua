e086b2439861a9e37f1139ee545986d0bc2a9c61
1054096100 <wushuang.nji@gmail.com>
2020-12-13 02:33:01 +0800

    修改，添加了可选清晰度的功能；添加了对视频名称编码的判断；添加了对视频名称的爬取与预装

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # xigua
-解析西瓜视频地址并下载
+解析西瓜视频地址并下载(可选择清晰度/预先设定标题)
+
+## Requirements
+
+- python >= 3.6
+
+```bash
+python -m pip -r requirements.txt
+```
+
+## Usage
+
+```bash
+python xigua.py
+# 直接回车测试 https://www.ixigua.com/i6704446868685849092
+输入西瓜链接：https://www.ixigua.com/6903456861337584142
+# 开始解析
+# Auto（输入 0 ）
+# 360p （输入 1 ） :  640 x 360
+# 480p （输入 2 ） :  854 x 480
+# 720p （输入 3 ） :  1280 x 720
+选择清晰度：0
+# http://v3-default.ixigua.com/c561bad6191712f71aebe719d65d7ace/5fd51b1f/video/tos/cn/tos-cn-ve-4/ee826b26c74f45c399a3e170f0778083/?a=2012&br=3636&bt=1212&cd=0%7C0%7C0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202012130221380102040550150E3E8569&lr=&mime_type=video_mp4&qs=0&rc=M3R2NHhuPDd5eTMzaTczM0ApZ2Y0NWg6PGVnNzk5ODQzOWcvZ2toZzY2bjNfLS1fLTBzc2AyYi02Y180LWItNmJiYjI6Yw%3D%3D&vl=&vr=
+# 开始下载
+# 【反击篇】1400亿，8次反转的中国6方财阀内斗：万科争夺战三部曲 - 西瓜视频.mp4下载33.56%---18.21M/s
+# 【反击篇】1400亿，8次反转的中国6方财阀内斗：万科争夺战三部曲 - 西瓜视频.mp4下载58.30%---13.42M/s
+# 【反击篇】1400亿，8次反转的中国6方财阀内斗：万科争夺战三部曲 - 西瓜视频.mp4下载81.73%---12.71M/s
+# 下载完成
+```
+
+默认下载在当前目录。
+
+基于 [xigua](https://github.com/py-wuhao/xigua)
+
--- video_parse.py
+++ video_parse.py
@@ -2,6 +2,11 @@
 author: wuaho
 email: 15392746632@qq.com
 data:2019-11-14
+
+#########################
+
+edited on 2020-12-13 by wushuang.yoyo (email: wushuang.nji@gmail.com )
+
 """
 import base64
 import json
@@ -11,6 +16,7 @@ import re
 import time
 import urllib.parse
 import zlib
+import chardet
 
 import requests
 from faker import Faker
@@ -24,8 +30,11 @@ class VideoParse:
     西瓜视频解析
     """
 
-    def __init__(self):
+    def __init__(self, url: str):
         self._faker = Faker()
+        self.ucontent = self._get_url(url)
+        self.vcontent = ''
+        self.def_dict = {}
 
     def _get_url(self, url: str) -> str:
         headers = {
@@ -36,11 +45,25 @@ class VideoParse:
             return response.text
         return ''
 
-    def get_video_id(self, url: str) -> str:
-        content = self._get_url(url)
-        video_id = re.findall(r'"vid":"([\w\d]+)"', content)
+    def get_video_id(self) -> str:
+        video_id = re.findall(r'"vid":"([\w\d]+)"', self.ucontent)
         return video_id[0] if video_id else ''
-
+    
+    def get_video_title(self) -> str:
+        video_title = re.findall(r'<title data-react-helmet=\"true\">([\s\S]+)</title>\n', self.ucontent)
+        return (video_title[0].encode("raw_unicode_escape").decode("utf-8") \
+                if chardet.detect(video_title[0].encode("raw_unicode_escape"))['encoding'] == 'utf-8' \
+                else video_title[0]) if video_title else ''
+    
+    def list_all_definition(self):
+        content_dict = json.loads(self.vcontent)
+        def_cnt = 0 # 计数
+        print('Auto（输入', def_cnt, '）')
+        for v in content_dict['data']['video_list'].values():
+            def_cnt += 1
+            print(v['definition'], "（输入", def_cnt, "） : ", v['vwidth'], "x", v['vheight'])
+            self.def_dict[def_cnt] = v
+    
     @staticmethod
     def get_main_url(video_id: str) -> str:
         r = ''.join(random.choices('0123456789', k=16))
@@ -50,21 +73,23 @@ class VideoParse:
         return url
 
     def get_video_url(self, source_url, try_num=3):
-        video_id = self.get_video_id(source_url)
+        video_id = self.get_video_id()
         url = self.get_main_url(video_id)
-        content = self._get_url(url)
-        content_dict = json.loads(content)
-        if content_dict.get('code') != 0:
+        self.vcontent = self._get_url(url)
+        content = json.loads(self.vcontent)
+        if content.get('code') != 0:
             if try_num > 0:
                 time.sleep(2)
                 return self.get_video_url(source_url, try_num - 1)
             else:
-                print(content_dict.get('message'))
+                print(content.get('message'))
                 return ''
         else:
-            main_url = content_dict['data']['video_list']['video_1']['main_url']
-            video_url = base64.b64decode(main_url).decode()
-            return video_url
+            self.list_all_definition()
+            def_opt = input("选择清晰度：")
+            if def_opt != '': def_opt = int(def_opt)
+            if def_opt in list(self.def_dict.values()): return base64.b64decode(self.def_dict[def_opt]['main_url']).decode()
+            elif def_opt == 0 or def_opt == '': return base64.b64decode(list(self.def_dict.values())[-1]['main_url']).decode() # 选了 Auto 的情况
 
 
 def download_file(name, url):
@@ -94,11 +119,11 @@ if __name__ == '__main__':
     source_url = input('输入西瓜链接：')
     if not source_url:
         source_url = 'https://www.ixigua.com/i6704446868685849092'
-    video_name = urllib.parse.urlparse(source_url).path.strip('/') + '.mp4'
-
-    video_parse = VideoParse()
+            
+    video_parse = VideoParse(source_url)
     print('开始解析')
     video_url = video_parse.get_video_url(source_url)
     print(video_url)
+    video_name = video_parse.get_video_title() + '.mp4'
     download_file(video_name, video_url)
     # os.system('pause')
